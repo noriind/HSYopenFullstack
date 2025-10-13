226d441a92b12abf9bb4b763318e467cb2342f5f
Noriin <103928163+noriind@users.noreply.github.com>
2025-10-13 13:02:10 +0300

    2.3: kurssitiedot step8

--- assignments/osa2/kurssitiedot/src/components/Course.jsx
+++ assignments/osa2/kurssitiedot/src/components/Course.jsx
@@ -1,29 +1,32 @@
-const Header = (props) => <h1>{props.course}</h1>
+const Header = (props) => <h1>{props.course}</h1>;
 
 const Content = (props) => (
-  <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-  </div>
-)
+    <div>
+        <Part part={props.parts[0]} />
+        <Part part={props.parts[1]} />
+        <Part part={props.parts[2]} />
+    </div>
+);
 
 const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
+    <p>
+        {props.part.name} {props.part.exercises}
+    </p>
+);
+
+const Total = (props) => {
+    const total = props.parts.reduce((sum, part) => sum + part.exercises, 0);
+    return <p>Total of excercises {total}</p>;
+};
 
 const Course = (props) => {
     return (
         <div>
-        <Header course={props.course.name} />
-        <Content parts={props.course.parts} />
-        <Total total={props.course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
+            <Header course={props.course.name} />
+            <Content parts={props.course.parts} />
+            <Total parts={props.course.parts} />
         </div>
-    )
-    }
-
-const Total = (props) => <p>Total of exercises {props.total}</p>
+    );
+};
 
 export default Course;
