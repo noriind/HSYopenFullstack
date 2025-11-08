12fc46175e17e4d60503b0abada5ff453196f536
Noriin <103928163+noriind@users.noreply.github.com>
2025-11-08 09:44:37 +0200

    3.2-3.5: puhelinluettelo backend steps 2,3,4,5

--- assignments/osa3/puhelinluettelo_backend/index.js
+++ assignments/osa3/puhelinluettelo_backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     id: "1",
@@ -29,8 +31,53 @@ let persons = [
   }
 ]
 
+const generateId = () => {
+  return String(Math.floor(Math.random()*1000000))
+}
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const date = new Date()
+  const personsCount = persons.length
+  response.send(`
+    <p>Phonebook has info for ${personsCount} people</p>
+    <p>${date}</p>
+    `)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
