8f6087ebefcd64e303ae40554fb1e99c2c5a7594
Noriin <103928163+noriind@users.noreply.github.com>
2025-12-25 13:47:43 +0200

    3.11 puhelinluettelo fullstack

--- assignments/osa3/puhelinluettelo_backend/index.js
+++ assignments/osa3/puhelinluettelo_backend/index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (request, response) =>{
   return JSON.stringify(request.body)
