913bc9f8f48878e7ab8343965b316e3b5ad73411
Noriin <103928163+noriind@users.noreply.github.com>
2025-12-26 21:30:37 +0200

    3.10:puhelinluettelo ja tietokanta step8

--- assignments/osa3/puhelinluettelo_backend/models/person.js
+++ assignments/osa3/puhelinluettelo_backend/models/person.js
@@ -15,17 +15,24 @@ mongoose
         console.log("error connecting to MongoDB:", error.message);
     });
 
-const personSchema = new mongoose.Schema({
-    name: {
+    const personSchema = new mongoose.Schema({
+      name: {
         type: String,
         minLength: 3,
-        required: true,
-    },
-    number: {
+        required: true
+      },
+      number: {
         type: String,
+        minLength: 8,
         required: true,
-    },
-});
+        validate: {
+          validator: function(v) {
+            return /^\d{2,3}-\d+$/.test(v)
+          },
+          message: props => `${props.value} is not a valid phone number! Format should be XX-XXXXXXX or XXX-XXXXXXX`
+        }
+      }
+    })
 
 personSchema.set("toJSON", {
     transform: (document, returnedObject) => {
