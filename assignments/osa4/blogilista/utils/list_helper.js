06cb1ac05b56a9ccb09ab683a975a81ac241e9f1
Noriin <103928163+noriind@users.noreply.github.com>
2025-12-27 11:09:37 +0200

    4.5-4.7: apufunktiot ja testit

--- assignments/osa4/blogilista/utils/list_helper.js
+++ assignments/osa4/blogilista/utils/list_helper.js
@@ -1,12 +1,77 @@
 const dummy = (blogs) => {
     return 1
   }
-
-const totalLikes = (blogs) => {
-    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
-}
   
-module.exports = {
+  const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  }
+  
+  const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+      return null
+    }
+  
+    const favorite = blogs.reduce((prev, current) => {
+      return (current.likes > prev.likes) ? current : prev
+    })
+  
+    return {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes
+    }
+  }
+  
+  const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+      return null
+    }
+  
+    const blogCounts = {}
+    
+    blogs.forEach(blog => {
+      blogCounts[blog.author] = (blogCounts[blog.author] || 0) + 1
+    })
+  
+    const authors = Object.keys(blogCounts)
+    const topAuthor = authors.reduce((prev, current) => {
+      return blogCounts[current] > blogCounts[prev] ? current : prev
+    })
+  
+    return {
+      author: topAuthor,
+      blogs: blogCounts[topAuthor]
+    }
+  }
+  
+  const mostLikes = (blogs) => {
+    if (blogs.length === 0) {
+      return null
+    }
+  
+    //tykkäysten yhteismäärä per kirjoittaja
+    const likesByAuthor = {}
+    
+    blogs.forEach(blog => {
+      likesByAuthor[blog.author] = (likesByAuthor[blog.author] || 0) + blog.likes
+    })
+  
+    //kirjoittaja jonka blogeilla eniten tykkäyksiä
+    const authors = Object.keys(likesByAuthor)
+    const topAuthor = authors.reduce((prev, current) => {
+      return likesByAuthor[current] > likesByAuthor[prev] ? current : prev
+    })
+  
+    return {
+      author: topAuthor,
+      likes: likesByAuthor[topAuthor]
+    }
+  }
+  
+  module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
   }
